3d6e001ad56714027ab2cae402a8cd08705d4367
ColtonKrushel <68721352+ColtonKrushel@users.noreply.github.com>
2021-11-29 23:25:05 -0800

    Pathfinding

---
+++ Assets/Pathfinding/GridManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] Vector2Int GridSize;
+    Dictionary<Vector2Int, Node> Grid = new Dictionary<Vector2Int, Node>();
+
+    void Awake()
+    {
+        CreateGrid();
+    }
+
+    void CreateGrid()
+    {
+        for(int x = 0; x < GridSize.x; x++)
+        {
+            for(int y = 0; y < GridSize.y; y++)
+            {
+                Vector2Int coordinates = new Vector2Int(x, y);
+                Grid.Add(coordinates, new Node(coordinates, true));
+            }
+        }
+    }
+}
